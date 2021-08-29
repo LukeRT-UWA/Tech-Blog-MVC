4c3a16a09cc71a000e230e5a8c3bcf620db4a592
Luke Thompson <16175583curtin@gmail.com>
2021-08-29 15:58:25 +0800

    fix packages

--- public/js/login.js
+++ public/js/login.js
@@ -34,13 +34,13 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      if (response.ok) {
-      document.location.replace('/');}
+      // const response = await fetch('/api/users/login', {
+      //   method: 'POST',
+      //   body: JSON.stringify({ email, password }),
+      //   headers: { 'Content-Type': 'application/json' },
+      // });
+      // if (response.ok) {
+      // document.location.replace('/');}
     } else {
       alert('Failed to sign up.');
     }
