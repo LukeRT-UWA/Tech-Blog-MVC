1832c7a8d90221d12717b12a99230c54ee52013d
Luke Thompson <16175583curtin@gmail.com>
2021-08-29 11:54:39 +0800

    add 404 route

--- server.js
+++ server.js
@@ -36,6 +36,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/404.html'))
+);
+
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
