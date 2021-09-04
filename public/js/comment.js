c6f9c7850ff8d222a06fde6f4f8a27fdab44f625
Luke Thompson <16175583curtin@gmail.com>
2021-09-04 14:41:06 +0800

    remove title from comment and associated

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,22 +1,18 @@
 const commentAdd = async (event) => {
     event.preventDefault();
-    console.log("commentJS")
-    const title = document.querySelector('#comment-title').value.trim();
     const content = document.querySelector('#comment-text').value.trim();
   
     const blog_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
-
     
-
     console.log(blog_id)
 
-    if (title && content) {
+    if (content) {
       
       const response = await fetch(`/api/comments`, {
         method: 'POST',
-        body: JSON.stringify({ title, content, blog_id }),
+        body: JSON.stringify({ content, blog_id }),
         headers: {
           'Content-Type': 'application/json',
         },
